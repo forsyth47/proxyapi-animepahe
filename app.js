2d0b1917f8140268b518922ac1fa64c74c17427b
Reiji <88034696+forsyth47@users.noreply.github.com>
2023-10-07 06:35:24 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.get("/animepahe.mp4", (req, res) => {
+  const link = req.query.url;
+  if (!link) {
+    return res.status(400).send("Missing 'url' query parameter.");
+  }
+
+  // You can add any additional headers you need here
+  const headers = {
+    "Content-Type": "video/mp4",
+  };
+
+  // Redirect to the provided URL with the specified headers
+  res.set(headers);
+  res.redirect(302, link);
+});
+
+// Listen on port 3000 (adjust as needed)
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
